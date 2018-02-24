66ea40ac6dd3362eabebc8c1ccaf9fac8aa4ee82
yuyang01 <alwyuyang@gmail.com>
2018-02-24 20:59:31 +0100

    openmp usage for c++ added

---
+++ openmp_cpp_examples/main.cpp
@@ -0,0 +1,34 @@
+
+#include<iostream>
+//#include<armadillo>
+#include<omp.h>
+
+using namespace std;
+//using namespace arma;
+
+
+int main()
+  {
+      
+  
+int nthreads, tid;
+omp_set_num_threads(4);
+/* Fork a team of threads giving them their own copies of variables */
+#pragma omp parallel private(nthreads, tid)
+  {
+
+  /* Obtain thread number */
+  tid = omp_get_thread_num();
+  printf("Hello World from thread = %d\n", tid);
+
+  /* Only master thread does this */
+  if (tid == 0) 
+    {
+    nthreads = omp_get_num_threads();
+    printf("Number of threads = %d\n", nthreads);
+    }
+
+  }  /* All threads join master thread and disband */
+  
+  return 0;
+  }
